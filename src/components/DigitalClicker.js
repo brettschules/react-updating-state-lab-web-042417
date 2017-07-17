daae68ee8c0c4827b9e9c31c6247e5a852d8beaf
Brett Schules <brettschules@gmail.com>
2017-07-17 15:33:53 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  clicky = () => {
+    this.setState({
+      timesClicked: ++this.state.timesClicked
+    })
+  }
+
+
+  render() {
+    console.log(this.state.timesClicked)
+    return(
+        <button onClick={this.clicky}>
+        </button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,34 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      js: { errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+    }
+  }
+  clickBitrate() {
+
+  }
+
+  clickResolution() {
+
+  }
+
+  render() {
+    return(
+      <div>
+      as
+      </div>
+    )
+  }
+}
